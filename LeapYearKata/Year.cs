cad1113960752653154cab77fe3fb653179a57a4
nicopaez <nicopaez@computer.org>
2016-07-31 17:23:39 -0300

    Year class added

---
+++ LeapYearKata/Year.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeapYearKata
+{
+    internal class Year
+    {
+        private int v;
+
+        public Year(int v)
+        {
+            this.v = v;
+        }
+
+        internal bool? IsLeap()
+        {
+            return false;
+        }
+    }
+}
---
+++ LeapYearKata/YearTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeapYearKata
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class YearTest
+    {
+        [Test]
+        public void IsNotLeapWhenIsNotDivisableBy4()
+        {
+            Assert.IsFalse(new Year(2001).IsLeap());
+        }
+    }
+}
