38603696cc314d629b6a20215460a95bafc62ad6
nicopaez <nicopaez@computer.org>
2016-07-31 17:33:09 -0300

    IsLeap completed

--- LeapYearKata/Year.cs
+++ LeapYearKata/Year.cs
@@ -13,7 +13,15 @@ namespace LeapYearKata
 
         public bool IsLeap()
         {
-            return (this.value % 4 == 0);
+            if (this.value % 4 == 0)
+            {
+                if (this.value % 100 == 0)
+                {
+                    return (this.value % 400 == 0);
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
--- LeapYearKata/YearTest.cs
+++ LeapYearKata/YearTest.cs
@@ -15,7 +15,12 @@
         public void IsLeapWhenIsDivisibleBy4AndNotBy100()
         {
             Assert.IsTrue(new Year(1996).IsLeap());
+        }
 
+        [Test]
+        public void IsNotLeapWhenIsDivisableBy4By100AndNotBy400()
+        {
+            Assert.IsFalse(new Year(1900).IsLeap());
         }
     }
 }
